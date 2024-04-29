ef3f9e2634bd6226a2374d1ba7fa10c9b7418d2d
mshoaib1244 <shoaib1244@gmail.com>
2024-04-29 19:43:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b = 2;
+var LogicalAnd = (a > 0) && (b > 0); //true
+console.log(LogicalAnd);
+var LogicalOr = (a < 0) || (b > 0); //true
+console.log(LogicalOr);
+var logicalNot = !(a > 0); //false
+console.log(logicalNot);
---
+++ main.ts
@@ -0,0 +1,11 @@
+let a: number = 5;
+let b: number = 2;
+
+let LogicalAnd = (a > 0) && (b > 0); //true
+console.log(LogicalAnd);
+let LogicalOr = (a < 0) || (b > 0); //true
+console.log(LogicalOr);
+let logicalNot = !(a > 0); //false
+console.log(logicalNot)
+
+
